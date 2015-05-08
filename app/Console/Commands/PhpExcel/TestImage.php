be516dcb9ebaf81d65bbdc6c8af681244066ef92
Eugeny Kostarev <eugeny.kostarev@gmail.com>
2015-05-08 17:10:13 +0500

    phpexcel iterator

--- app/Console/Commands/PhpExcel/TestImage.php
+++ app/Console/Commands/PhpExcel/TestImage.php
@@ -37,6 +37,21 @@ class TestImage extends Command
      */
     public function fire()
     {
-        $this->info(storage_path());
+        $path = storage_path() . '/app/phpexcel/test.xls';
+        $this->info('Path: ' . $path);
+        $phpExcel = PHPExcel_IOFactory::load($path);
+        foreach ($phpExcel->getWorksheetIterator() as $worksheet) {
+            $this->info('  Worksheet: ' . $worksheet->getTitle());
+            foreach ($worksheet->getRowIterator() as $row) {
+                $this->info('    Row number: ' . $row->getRowIndex());
+                foreach ($row->getCellIterator() as $cell) {
+                    if (!is_null($cell)) {
+                        $this->info('      Cell: ' . $cell->getCoordinate() . '; Value: ' . $cell->getCalculatedValue());
+                    } else {
+                        $this->error('      Cell: ' . $cell->getCoordinate() . '; Value: null');
+                    }
+                }
+            }
+        }
     }
 }
